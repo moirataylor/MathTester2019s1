0a3b0084d051a573171f49bbb3dbc15c047b96dc
moira <moira.taylor.2023@student.lhschools.org>
2019-09-12 09:32:28 -0400

    Initial commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,27 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String firstWord;
+        String secondWord;
+        int firstNumber;
+        int secondNumber;
+
+        firstWord = "dog";
+        secondWord = "house";
+        firstNumber = 25;
+        secondNumber = 10;
+
+        System.out.println(firstWord+secondWord);
+        System.out.println(firstNumber+secondNumber);
+        System.out.println(firstWord+(firstNumber+secondNumber));
+
+
+
+
+
+
+    }
+
+}
