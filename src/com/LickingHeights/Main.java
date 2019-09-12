17e260c0a54a86cfb95abf6a4524149dcc6af3f5
moira <moira.taylor.2023@student.lhschools.org>
2019-09-12 10:11:00 -0400

    declaring 'int' variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,10 +5,14 @@ public class Main {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
+        int firstNumber,secondNumber;
 
         firstWord = "dog";
         secondWord = "house";
-        
+
+        System.out.println(firstWord+secondWord);
+
+
 
 
 
