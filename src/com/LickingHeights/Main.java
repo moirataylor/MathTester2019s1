5d6aef10ad7a142d3acf32fdf617956652ff1df8
moira <moira.taylor.2023@student.lhschools.org>
2019-09-12 10:01:41 -0400

    declaring variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,18 +5,6 @@ public class Main {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
-        int firstNumber;
-        int secondNumber;
-
-        firstWord = "dog";
-        secondWord = "house";
-        firstNumber = 25;
-        secondNumber = 10;
-
-        System.out.println(firstWord+secondWord);
-        System.out.println(firstNumber+secondNumber);
-        System.out.println(firstWord+(firstNumber+secondNumber));
-
 
 
 
