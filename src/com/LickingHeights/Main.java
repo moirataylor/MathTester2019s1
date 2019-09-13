44a50fdb8c917d4d35e3ab3368c0de344df143be
moira <moira.taylor.2023@student.lhschools.org>
2019-09-13 09:27:21 -0400

    put operations into block comment and added total variable

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,20 +5,25 @@ public class Main {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
-        int firstNumber,secondNumber;
+        int firstNumber,secondNumber,total;
 
         firstWord = "dog";
         secondWord = "house";
         firstNumber = 5;
         secondNumber = 10;
+        total = firstNumber + secondNumber;
+        System.out.println(total);
+        total = total + 10;
+        System.out.println(total);
 
-        System.out.println(firstWord+secondWord);
+       /* System.out.println(firstWord+secondWord);
         System.out.println(firstWord+firstNumber);
         System.out.println(firstWord+(firstNumber+secondNumber));
         System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
         System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
-        System.out.println("The modulus of the secondNumber by the firstNumber is "+ (secondNumber%firstNumber));
+        System.out.println("The modulus of the secondNumber by the firstNumber is "+ (secondNumber%firstNumber));*/
+
 
 
 
