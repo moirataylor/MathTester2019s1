3eb352b483b67fe5e468b5db6955d7373919c579
moira <moira.taylor.2023@student.lhschools.org>
2019-09-13 09:20:07 -0400

    using operations with first and second number
    addition,subtraction,multiplication,division,modulus

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -15,7 +15,10 @@ public class Main {
         System.out.println(firstWord+secondWord);
         System.out.println(firstWord+firstNumber);
         System.out.println(firstWord+(firstNumber+secondNumber));
-
+        System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
+        System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
+        System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
+        System.out.println("The modulus of the secondNumber by the firstNumber is "+ (secondNumber%firstNumber));
 
 
 
